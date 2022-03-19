336a32370c8cae4eede9b2bbb7a848209260a013
PranavPurwar <purwarpranav80@gmail.com>
2022-03-19 13:02:21 +0530

    add whole project

--- lib-android/src/main/java/com/pranav/java/lib_android/CompilationFailedException.kt
+++
@@ -1,3 +0,0 @@
-package com.pranav.java.lib_android
-
-class CompilationFailedException(override val message: String?) : Throwable()
--- lib-android/src/main/java/com/pranav/java/lib_android/Task.kt
+++
@@ -1,7 +0,0 @@
-package com.pranav.java.lib_android
-
-abstract class Task {
-    abstract fun doFullTask()
-
-    abstract fun getTaskName(): String
-}

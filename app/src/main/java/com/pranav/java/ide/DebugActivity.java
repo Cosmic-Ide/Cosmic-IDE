80e40fc02f7349ae2c5b8a5c1987b74f8d3165dd
PranavPurwar <purwarpranav80@gmail.com>
2022-03-25 13:04:09 +0530

    remove unnecessary detailed error message

--- app/src/main/java/com/pranav/java/ide/DebugActivity.java
+++ app/src/main/java/com/pranav/java/ide/DebugActivity.java
@@ -5,65 +5,21 @@ import androidx.appcompat.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 public class DebugActivity extends AppCompatActivity {
-
-    private String[] exceptionTypes = {
-        "StringIndexOutOfBoundsException",
-        "IndexOutOfBoundsException",
-        "ArithmeticException",
-        "NumberFormatException",
-        "ActivityNotFoundException"
-    };
-
-    private String[] exceptionMessages = {
-        "Invalid string operation\n",
-        "Invalid list operation\n",
-        "Invalid arithmetical operation\n",
-        "Invalid toNumber block operation\n",
-        "Invalid intent operation"
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
-        String errorMessage = "";
-        String madeErrorMessage = "";
-
-        if (intent != null) {
-            errorMessage = intent.getStringExtra("error");
-
-            String[] split = errorMessage.split("\n");
-            try {
-                for (int j = 0; j < exceptionTypes.length; j++) {
-                    if (split[0].contains(exceptionTypes[j])) {
-                        madeErrorMessage = exceptionMessages[j];
-
-                        int addIndex = split[0].indexOf(exceptionTypes[j]) + exceptionTypes[j].length();
-
-                        madeErrorMessage += split[0].substring(addIndex, split[0].length());
-                        madeErrorMessage += "\n\nDetailed error message:\n" + errorMessage;
-                        break;
-                    }
-                }
-
-                if (madeErrorMessage.isEmpty()) {
-                    madeErrorMessage = errorMessage;
-                }
-            } catch (Exception e) {
-                madeErrorMessage = madeErrorMessage + "\n\nError while getting error: " + Log.getStackTraceString(e);
-            }
-        }
+        String errorMessage = intent.getStringExtra("error");
 
         new AlertDialog.Builder(this)
-               .setTitle("An error occurred")
-               .setMessage(madeErrorMessage)
-               .setPositiveButton("End Application", (dialog, which) -> finish())
-               .create()
-               .show();
+           .setTitle("An error occurred")
+           .setMessage(errorMessage)
+           .setPositiveButton("End Application", (dialog, which) -> finish())
+           .create()
+           .show();
     }
 }

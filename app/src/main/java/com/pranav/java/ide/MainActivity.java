a541e2632feb5344e1e0d00634b7ab844dec472e
PranavPurwar <purwarpranav80@gmail.com>
2022-03-26 15:19:03 +0530

    feat: remove duplicate import

--- app/src/main/java/com/pranav/java/ide/MainActivity.java
+++ app/src/main/java/com/pranav/java/ide/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.os.Bundle;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;

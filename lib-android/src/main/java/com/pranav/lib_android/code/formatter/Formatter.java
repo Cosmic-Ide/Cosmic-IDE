3d2ef0726c3d14da732dcf53af259233dce190fe
PranavPurwar <purwarpranav80@gmail.com>
2022-04-09 22:00:46 +0530

    fix: revert files to java (i suck at kotlin ngl)

--- lib-android/src/main/java/com/pranav/lib_android/code/formatter/Formatter.java
+++ lib-android/src/main/java/com/pranav/lib_android/code/formatter/Formatter.java
@@ -20,7 +20,7 @@ public class Formatter {
 	}
 	
 	public String format() {
-		DefaultCodeFormatterOptions options = DefaultCodeFormatterConstants.getEclipse21Settings();
+		Map options = DefaultCodeFormatterConstants.getEclipse21Settings();
 		
 		options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_7);
 		options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM,

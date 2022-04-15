d776776348ada06e159256c05936b57eaa5ebb55
PranavPurwar <purwarpranav80@gmail.com>
2022-04-15 22:15:39 +0530

    fix: add class

--- lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
+++ lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
@@ -2,15 +2,17 @@ package com.pranav.lib_android.util
 
 import kotlinx.coroutines.*
 
-fun execute(runnable: Runnable) = runBlocking {
-  val deferred: Deferred<Unit> = async {
-    runnable.run()
+class ConcurrentUtil {
+  fun execute(runnable: Runnable) = runBlocking {
+    val deferred: Deferred<Unit> = async {
+      runnable.run()
+    }
+    deferred.await()
   }
-  deferred.await()
-}
 
-fun executeInBackground(runnable: Runnable) = runBlocking {
-  launch {
-    runnable.run()
+  fun executeInBackground(runnable: Runnable) = runBlocking {
+    launch {
+      runnable.run()
+    }
   }
 }

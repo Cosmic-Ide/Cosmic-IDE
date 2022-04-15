c9af2e773068f12d3b956e36707d2a1d30a98ac7
PranavPurwar <purwarpranav80@gmail.com>
2022-04-15 12:44:31 +0530

    chore: remove unused kotlin dependencies to reduce app size

--- app/src/androidTest/java/com/pranav/java/ide/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/pranav/java/ide/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.pranav.kotlin.ide", appContext.packageName)
+        assertEquals("com.pranav.java.ide", appContext.packageName)
     }
 }

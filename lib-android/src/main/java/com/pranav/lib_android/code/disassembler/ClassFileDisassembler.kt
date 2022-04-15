1d8704bbdccffd6ec9b7d265bb1a35d1bbe830af
PranavPurwar <purwarpranav80@gmail.com>
2022-04-15 21:50:04 +0530

    feat: convert files to kotlin

---
+++ lib-android/src/main/java/com/pranav/lib_android/code/disassembler/ClassFileDisassembler.kt
@@ -0,0 +1,19 @@
+package com.pranav.lib_android.code.disassembler
+
+import java.io.File
+
+import org.eclipse.jdt.internal.core.util.Disassembler
+
+class ClassFileDisassembler(classFile: String) {
+    
+    val classFileBytes: Array<Byte>
+    
+    init {
+      classFileBytes = File(classFile).readBytes()
+    }
+	
+	fun disassemble(): String {
+		return Disassembler()
+		    .disassemble(classFileBytes, System.lineSeparator())
+	}
+}
---
+++ lib-android/src/main/java/com/pranav/lib_android/code/formatter/Formatter.kt
@@ -0,0 +1,41 @@
+package com.pranav.lib_android.code.formatter
+
+import com.pranav.lib_android.util.ConcurrentUtilKt
+import org.eclipse.jdt.internal.formatter.DefaultCodeFormatterOptions
+import org.eclipse.jdt.internal.formatter.DefaultCodeFormatter
+import org.eclipse.jface.text.Document
+
+class Formatter(javaSource: String) {
+	
+	val source: String
+	
+	init {
+		source = javaSource
+	}
+	
+	fun format(): String {
+		val options = DefaultCodeFormatterOptions.getEclipseDefaultSettings()
+		
+		val codeFormatter = DefaultCodeFormatter(options)
+		
+		val edit = codeFormatter.format(
+	    	DefaultCodeFormatter.K_COMPILATION_UNIT,
+	    	source,
+    		0, // starting index
+    		source.length(), // length
+    		0, // initial indentation
+	    	System.lineSeparator() // line separator
+		)
+		
+		val document = Document(source)
+		ConcurrentUtilKt.execute(() -> {
+			try {
+				edit.apply(document)
+			} catch (Exception e) {
+				throw IllegalStateException(e)
+			}
+		});
+		// return the formatted code
+		return document.get()
+	}
+}

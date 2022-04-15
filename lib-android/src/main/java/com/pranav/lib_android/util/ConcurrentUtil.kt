0e5287f4662cdfcda3fb8a02c4aace6936de1487
PranavPurwar <purwarpranav80@gmail.com>
2022-04-15 22:32:59 +0530

    add pseudo init method

--- lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
+++ lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
@@ -3,16 +3,19 @@ package com.pranav.lib_android.util
 import kotlinx.coroutines.*
 
 class ConcurrentUtil {
-  fun execute(runnable: Runnable) = runBlocking {
+
+  init {
+    // empty
+  }
+
+  suspend fun execute(runnable: Runnable) {
     val deferred: Deferred<Unit> = async {
       runnable.run()
     }
     deferred.await()
   }
 
-  fun executeInBackground(runnable: Runnable) = runBlocking {
-    launch {
-      runnable.run()
-    }
+  suspend fun executeInBackground(runnable: Runnable) {
+    runnable.run()
   }
 }

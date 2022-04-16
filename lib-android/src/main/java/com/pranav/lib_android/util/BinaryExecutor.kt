0bcc58861ef1fdd56699357bc05e93cc3c0cd976
PranavPurwar <purwarpranav80@gmail.com>
2022-04-16 18:44:17 +0530

    you

---
+++ lib-android/src/main/java/com/pranav/lib_android/util/BinaryExecutor.kt
@@ -0,0 +1,54 @@
+package com.pranav.lib_android.util
+
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.util.ArrayList
+import java.util.Scanner
+
+class BinaryExecutor {
+
+  var mProcess = ProcessBuilder()
+  var mWriter = StringWriter()
+
+  fun execute(arrayList: ArrayList<String>): String {
+		mProcess.command(arrayList)
+    try {
+			val process = mProcess.start()
+      val scanner = Scanner(process.getErrorStream())
+      while (scanner.hasNextLine()) {
+        mWriter.append(scanner.nextLine())
+        mWriter.append(System.lineSeparator())
+      }
+
+			process.waitFor()
+    } catch (e: Exception) {
+      e.printStackTrace(PrintWriter(mWriter))
+    }
+    return mWriter.toString()
+  }
+
+	fun execute(command: String): String {
+		mProcess.command(command)
+    try {
+			val process = mProcess.start()
+      val scanner = Scanner(process.getErrorStream())
+      while (scanner.hasNextLine()) {
+        mWriter.append(scanner.nextLine())
+        mWriter.append(System.lineSeparator())
+      }
+
+			process.waitFor()
+    } catch (e: Exception) {
+      e.printStackTrace(PrintWriter(mWriter))
+    }
+    return mWriter.toString()
+	}
+
+  fun getLogs(): String {
+    return mWriter.toString()
+  }
+
+	fun clear() {
+		mWriter.flush()
+	}
+}
---
+++ lib-android/src/main/java/com/pranav/lib_android/util/FileUtil.kt
@@ -0,0 +1,42 @@
+package com.pranav.lib_android.util
+
+import android.content.Context
+
+import java.io.File
+
+class FileUtil {
+
+  companion object {
+    var mContext: Context
+
+    @JvmStatic
+    fun initializeContext(context: Context) {
+      mContext = context
+    }
+
+    @JvmStatic
+    fun deleteFile(String path) {
+      File(path).deleteRecursively()
+    }
+
+    @JvmStatic
+    fun getDataDir(): String {
+      return mContext.getExternalFilesDir(null).getAbsolutePath()
+    }
+
+    @JvmStatic
+    fun getJavaDir(): String {
+      return getDataDir() + "/java/"
+    }
+
+    @JvmStatic
+    fun getBinDir(): String {
+      return getDataDir() + "/bin/"
+    }
+
+    @JvmStatic
+    fun getClasspathDir(): String {
+      return getDataDir() + "/classpath/"
+    }
+  }
+}

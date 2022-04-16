d39c405697712e4c0d094dcbf747c20ca585be71
PranavPurwar <purwarpranav80@gmail.com>
2022-04-16 14:51:46 +0530

    up

--- lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
+++ lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
@@ -2,18 +2,21 @@ package com.pranav.lib_android.util
 
 import kotlinx.coroutines.*
 
-object ConcurrentUtil {
-
-  fun execute(runnable: Runnable) = runBlocking {
-    val deferred: Deferred<Unit> = async {
-      runnable.run()
+class ConcurrentUtil {
+  companion object {
+    @JvmStatic
+    fun execute(runnable: Runnable) = runBlocking {
+      val deferred: Deferred<Unit> = async {
+        runnable.run()
+      }
+      deferred.await()
     }
-    deferred.await()
-  }
 
-  fun executeInBackground(runnable: Runnable) = runBlocking {
-    launch {
-      runnable.run()
+    @JvmStatic
+    fun executeInBackground(runnable: Runnable) = runBlocking {
+      launch {
+        runnable.run()
+      }
     }
   }
 }

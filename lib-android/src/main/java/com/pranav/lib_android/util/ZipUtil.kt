8839177e2dc87e3088f4d50e20edfd032814b716
PranavPurwar <purwarpranav80@gmail.com>
2022-04-17 20:48:12 +0530

    fix

--- lib-android/src/main/java/com/pranav/lib_android/util/ZipUtil.kt
+++ lib-android/src/main/java/com/pranav/lib_android/util/ZipUtil.kt
@@ -1,59 +1,66 @@
-package com.pranav.lib_android.util;
+package com.pranav.lib_android.util
 
-import java.io.*
-import java.util.zip.ZipFile
 import android.content.Context
+import android.util.Log
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 class ZipUtil {
+	private val BUFFER_SIZE = 1024 * 10
+	private val TAG = "ZipUtil"
 
-  companion object {
-
-  val BUFFER_SIZE = 4096
-
-  @JvmStatic
-  fun unzipFromAssets(context: Context, fileName: String, outputDir: String) {
-    copyFileFromAssets(context, fileName, outputDir)
-    unzip(File(outputDir, fileName), outputDir)
-    File(outputDir, fileName).delete()
-  }
-
-  @JvmStatic
-  fun unzip(zipFilePath: File, destDirectory: String) {
-    File(destDirectory).run {
-      if (!exists())
-        mkdirs()
-    }
-
-    ZipFile(zipFilePath).use { zip ->
-      zip.entries().asSequence().forEach { entry ->
-        zip.getInputStream(entry).use { input ->
-          val output = destDirectory + File.separator + entry.name
-          if (entry.isDirectory) {
-            File(output).mkdir()
-          } else {
-            extractFile(input, output)
-          }
-          input.close()
-        }
-      }
-    }
-  }
-
-  @JvmStatic
-  fun extractFile(inputStream: InputStream, destFilePath: String) {
-    val output = File(destFilePath)
-    output.writeBytes(inputStream.readBytes())
-  }
-
-  @JvmStatic
-	fun copyFileFromAssets(context: Context, inputFile: String, outputDir: String) {
-		val input = context.getAssets().`open`(inputFile)
-		val outputPath = outputDir + inputFile
-		val file = File(outputPath)
-		if (file.createNewFile()) {
-		  file.writeBytes(input.readBytes())
+	fun unzipFromAssets(context: Context, zipFile: String,
+	destination: String) {
+		try {
+			val stream = context.getAssets().`open`(zipFile)
+			unzip(stream, destination)
+		} catch (e: IOException) {
+			e.printStackTrace()
 		}
-		input.close()
 	}
+
+	private fun unzip(stream: InputStream, destination: String) {
+		dirChecker(destination, "")
+		val buffer = Byte[BUFFER_SIZE]
+		try {
+			val zin = ZipInputStream(stream)
+			var ze: ZipEntry = null
+
+			while ((var ze = zin.getNextEntry()) != null) {
+				if (ze.isDirectory()) {
+					dirChecker(destination, ze.getName())
+				} else {
+					val f = File(destination, ze.getName())
+					if (!f.normalize().startsWith(destination))
+						throw SecurityException("Potentially harmful files detected inside zip")
+					if (!f.exists()) {
+						val success = f.createNewFile()
+						if (!success) {
+							Log.w(TAG, "Failed to create file " + f.getName())
+							continue
+						}
+						f.appendBytes(zin.readBytes())
+						zin.closeEntry()
+						fout.close()
+					}
+				}
+			}
+			zin.close()
+		} catch (e: IOException) {
+			Log.e(TAG, "unzip", e)
+		}
+	}
+
+	private fun dirChecker(destination: String, dir: String) {
+		val f = File(destination, dir)
+
+		if (!f.isDirectory() && !f.mkdirs()) {
+				Log.w(TAG, "Failed to create folder " + f.getName())
+		}
 	}
 }

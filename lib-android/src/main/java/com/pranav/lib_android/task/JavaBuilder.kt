d1b702aee1e0cbab15e2493ef67f51ec7e59259b
PranavPurwar <purwarpranav80@gmail.com>
2022-04-17 15:11:33 +0530

    Constructor()

--- lib-android/src/main/java/com/pranav/lib_android/task/JavaBuilder.kt
+++ lib-android/src/main/java/com/pranav/lib_android/task/JavaBuilder.kt
@@ -3,16 +3,10 @@ package com.pranav.lib_android.task
 import android.content.Context
 import com.pranav.lib_android.interfaces.Builder
 
-class JavaBuilder(context: Context, loader: ClassLoader): Builder() {
-
-	var classloader: ClassLoader
-
-	var mContext: Context
-
-	init {
-		mContext = context
-		classloader = loader
-	}
+class JavaBuilder constructor(
+    val mContext: Context,
+    val classLoader: ClassLoader
+  ): Builder() {
 
 	override fun getContext() = mContext
 

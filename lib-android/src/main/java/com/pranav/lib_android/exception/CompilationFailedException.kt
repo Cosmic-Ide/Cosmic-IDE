95d4f9cdc9b9e3ab860b0fc56a0553f069629fc1
PranavPurwar <purwarpranav80@gmail.com>
2022-04-17 14:12:09 +0530

    fix

--- lib-android/src/main/java/com/pranav/lib_android/exception/CompilationFailedException.kt
+++ lib-android/src/main/java/com/pranav/lib_android/exception/CompilationFailedException.kt
@@ -1,12 +1,5 @@
 package com.pranav.lib_android.exception
 
-class CompilationFailedException(): Exception() {
-
-	constructor(e: Throwable?) {
-		super(e)
-	}
-
-	constructor(message: String?) {
-		super(message)
-	}
+class CompilationFailedException(e: Throwable?): Exception() {
+  // empty class
 }

6df7a3245f7a0b607adf2973d6dc16b00a22c9ef
PranavPurwar <purwarpranav80@gmail.com>
2022-04-18 15:03:09 +0530

    cleanup files

---
+++ app/app/src/androidTest/java/com/pranav/java/ide/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.pranav.java.ide
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+// Check the package name of project.
+	@Test
+	// Check if app context is accessible.
+	fun useAppContext() {
+		// Context of the app under test.
+		val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+		assertEquals("com.pranav.java.ide", appContext.packageName)
+	}
+}

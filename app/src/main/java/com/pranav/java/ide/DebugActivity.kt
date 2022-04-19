c34b708dba5cd497e6ce4581c11e014369d45659
PranavPurwar <purwarpranav80@gmail.com>
2022-04-19 18:56:08 +0530

    debug activity to kotlin

---
+++ app/src/main/java/com/pranav/java/ide/DebugActivity.kt
@@ -0,0 +1,23 @@
+package com.pranav.java.ide
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class DebugActivity: AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle) {
+		super.onCreate(savedInstanceState)
+
+		val intent = getIntent()
+		val errorMessage = intent.getStringExtra("error")
+
+		AlertDialog.Builder(this)
+				.setTitle("An error occurred")
+				.setMessage(errorMessage)
+				.setPositiveButton("Quit", {dialog, which -> finish()})
+				.create()
+				.show()
+	}
+}

ca546284fb9965f00f214bf94f9071ae271bae4b
PranavPurwar <purwarpranav80@gmail.com>
2022-04-21 14:44:06 +0530

    feat: make kotlin classes methods static and minor cleanup

--- lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
+++ lib-android/src/main/java/com/pranav/lib_android/util/ConcurrentUtil.kt
@@ -2,15 +2,22 @@ package com.pranav.lib_android.util
 
 import kotlinx.coroutines.*
 
-fun execute(runnable: Runnable) = runBlocking {
-	val deferred: Deferred<Unit> = async {
-		runnable.run()
-	}
-	deferred.await()
-}
+class ConcurrentUtil {
+  // make these utility methods static for java interoperability
+  companion object {
+    @JvmStatic
+    fun execute(runnable: Runnable) = runBlocking {
+      val deferred: Deferred<Unit> = async {
+        runnable.run()
+      }
+      deferred.await()
+    }
 
-fun executeInBackground(runnable: Runnable) = runBlocking {
-	launch {
-		runnable.run()
-	}
+    @JvmStatic
+    fun executeInBackground(runnable: Runnable) = runBlocking {
+      launch {
+        runnable.run()
+      }
+    }
+  }
 }

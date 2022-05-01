9537ff3c7671ec3b14fffe7b04c8fa1edea9bd49
PranavPurwar <purwarpranav80@gmail.com>
2022-05-01 22:18:56 +0530

    fix build errors

--- lib-android/src/main/java/com/pranav/lib_android/task/java/JavacTask.java
+++ lib-android/src/main/java/com/pranav/lib_android/task/java/JavacTask.java
@@ -7,18 +7,26 @@ import com.pranav.lib_android.exception.CompilationFailedException;
 import com.pranav.lib_android.interfaces.*;
 import com.pranav.lib_android.util.ConcurrentUtil;
 import com.pranav.lib_android.util.FileUtil;
+
 import com.sun.tools.javac.Main;
 import com.sun.tools.javac.api.JavacTool;
 import com.sun.tools.javac.file.JavacFileManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Collections;
 
-import javax.tools.DiagnosticListener;
+import javax.tools.JavaFileObject;
+import javax.tools.JavaCompiler;
+import javax.tools.DiagnosticCollector;
 import javax.tools.StandardLocation;
+import javax.tools.SourceFileObject;
 
 public class JavacTask extends Task {
 
@@ -45,67 +53,64 @@ public class JavacTask extends Task {
 
         ConcurrentUtil.execute(
                 () -> {
-                    DiagnosticListener<JavaFileObject> diagnosticCollector =
-                            diagnostic -> {
-                                switch (diagnostic.getKind()) {
-                                    case ERROR:
-                                        getLogger().error(new DiagnosticWrapper(diagnostic));
-                                        break;
-                                    case WARNING:
-                                        getLogger().warning(new DiagnosticWrapper(diagnostic));
-                                }
-                            };
+            DiagnosticCollector<JavaFileObject> diagnosticCollector = new DiagnosticCollector<>();
 
-                    List<JavaFileObject> javaFileObjects = new ArrayList<>();
-                    List<File> javaFiles = getSourceFiles(new File(FileUtil.getJavaDir()));
-                    for (File file : javaFiles) {
-                        javaFileObjects.add(new SourceFileObject(file.toPath()));
-                    }
+        List<JavaFileObject> javaFileObjects = new ArrayList<>();
+        List<File> javaFiles = getSourceFiles(new File(FileUtil.getJavaDir()));
+        for (File file : javaFiles) {
+            javaFileObjects.add(new SourceFileObject(file.toPath()));
+        }
 
-                    JavacTool tool = JavacTool.create();
+        JavacTool tool = JavacTool.create();
 
-                    JavacFileManager standardJavaFileManager =
-                            tool.getStandardFileManager(
-                                    diagnosticCollector,
-                                    Locale.getDefault(),
-                                    Charset.defaultCharset());
-                    try {
-                        standardJavaFileManager.setLocation(
-                                StandardLocation.CLASS_OUTPUT, Collections.singletonList(output));
-                        standardJavaFileManager.setLocation(
-                                StandardLocation.PLATFORM_CLASS_PATH,
-                                getPlatformClasspath(version));
-                        standardJavaFileManager.setLocation(
-                                StandardLocation.CLASS_PATH, getClassPath());
-                        standardJavaFileManager.setLocation(
-                                StandardLocation.SOURCE_PATH, javaFiles);
-                    } catch (IOException e) {
-                        throw new CompilationFailedException(e);
-                    }
+        JavacFileManager standardJavaFileManager = tool.getStandardFileManager(
+                diagnosticCollector,
+                Locale.getDefault(),
+                Charset.defaultCharset()
+        );
+        try {
+            standardJavaFileManager.setLocation(StandardLocation.CLASS_OUTPUT, Collections.singletonList(output));
+            standardJavaFileManager.setLocation(StandardLocation.PLATFORM_CLASS_PATH, getPlatformClasspath(version));
+            standardJavaFileManager.setLocation(StandardLocation.CLASS_PATH, getClasspath());
+            standardJavaFileManager.setLocation(StandardLocation.SOURCE_PATH, javaFiles);
+        } catch (IOException e) {
+            throw new CompilationFailedException(e);
+        }
 
-                    final ArrayList<String> args = new ArrayList<>();
+        final ArrayList<String> args = new ArrayList<>();
 
                     args.add("-g");
                     args.add("-source");
                     args.add(version);
                     args.add("-target");
                     args.add(version);
-
+                    
                     args.add("-proc:none");
 
-                    JavacTask task =
-                            tool.getTask(
-                                    null,
-                                    standardJavaFileManager,
-                                    diagnosticCollector,
-                                    args,
-                                    null,
-                                    javaFileObjects);
+        JavacTaskImpl task = (JavaTaskImpl) tool.getTask(
+                null,
+                standardJavaFileManager,
+                diagnosticCollector,
+                args,
+                null,
+                javaFileObjects
+        );
 
-                    if (!task.call()) {
-                        throw new CompilationFailedException(errs.toString());
-                    }
-                    Main.compile(args.toArray(new String[0]), writer);
+        if (!task.call()) {
+            throw new CompilationFailedException(errs.toString());
+        }
+        for (final Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+        switch (diagnostic.getKind()) {
+            case ERROR:
+          errs.append(diagnostic.getMessage(Locale.getDefault()));
+          errs.append("\n");
+          break;
+          case WARNING:
+          warnings.append(diagnostic.getMessage(Locale.getDefault()));
+          warnings.append("\n");
+          break;
+        }
+      }
                 });
         String errors = errs.toString();
 
@@ -113,12 +118,12 @@ public class JavacTask extends Task {
             throw new CompilationFailedException(errors);
         }
     }
-
+    
     public ArrayList<File> getSourceFiles(File path) {
         ArrayList<File> sourceFiles = new ArrayList<>();
         File[] files = path.listFiles();
         if (files == null) {
-            return Collections.emptyList();
+            return new ArrayList<File>();
         }
         for (File file : files) {
             if (file.isFile()) {
@@ -131,22 +136,22 @@ public class JavacTask extends Task {
         }
         return sourceFiles;
     }
-
+    
     public List<File> getClasspath() {
         List<File> classpath = new ArrayList<>();
         final StringBuilder path = new StringBuilder();
         final String clspath = prefs.getString("classpath", "");
-        if (!clspath.isEmpty() && classpath.length() > 0) {
+        if (!clspath.isEmpty() && path.length() > 0) {
             path.append(":");
-            cpath.append(clspath);
+            path.append(clspath);
         }
-
+        
         for (String clas : path.toString().split(":")) {
             classpath.add(new File(clas));
         }
         return classpath;
     }
-
+    
     public List<File> getPlatformClasspath(String version) {
         List<File> classpath = new ArrayList<>();
         classpath.add(new File(FileUtil.getClasspathDir(), "android.jar"));

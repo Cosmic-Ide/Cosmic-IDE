4beccb48a9c884d10058a9f1f5992cdf5e8f4f5d
PranavPurwar <purwarpranav80@gmail.com>
2022-05-01 12:34:30 +0530

    fix: don't close inputStream after use

--- lib-eclipse/src/main/java/org/eclipse/jdt/internal/compiler/util/Util.java
+++ lib-eclipse/src/main/java/org/eclipse/jdt/internal/compiler/util/Util.java
@@ -478,30 +478,13 @@ public class Util implements SuffixConstants {
             final int bufLen = 4 * 0x400; // 4KB
             byte[] buf = new byte[bufLen];
             int readLen;
-            IOException exception = null;
             
-            try {
-                try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-                    while ((readLen = inputStream.read(buf, 0, bufLen)) != -1) {
-                        outputStream.write(buf, 0, readLen);
-                    }
-                    return outputStream.toByteArray();
-                }
-            } catch (IOException e) {
-                exception = e;
-                throw e;
-            } finally {
-                if (exception == null) {
-                    inputStream.close();
-                } else {
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        exception.addSuppressed(e);
-                    }
-               }
-           }
-       }
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            while ((readLen = input.read(buf, 0, bufLen)) != -1) {
+                outputStream.write(buf, 0, readLen);
+            }
+            return outputStream.toByteArray();
+        }
     }
 
     /**

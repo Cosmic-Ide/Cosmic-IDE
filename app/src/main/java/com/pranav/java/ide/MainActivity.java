fa52e889f99004624708745d9af5db4d6098a9cb
PranavPurwar <purwarpranav80@gmail.com>
2022-05-05 07:38:54 +0530

    put setText() outside coroutines as suggested by mike

--- app/src/main/java/com/pranav/java/ide/MainActivity.java
+++ app/src/main/java/com/pranav/java/ide/MainActivity.java
@@ -196,19 +196,21 @@ public final class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.format_menu_button) {
+            String formattedCode = "";
             ConcurrentUtil.execute(
                     () -> {
                         if (prefs.getString("formatter", "Google Java Formatter")
                                 .equals("Google Java Formatter")) {
                             GoogleJavaFormatter formatter =
                                     new GoogleJavaFormatter(editor.getText().toString());
-                            editor.setText(formatter.format());
+                            formattedCode = formatter.format();
                         } else {
                             EclipseJavaFormatter formatter =
                                     new EclipseJavaFormatter(editor.getText().toString());
-                            editor.setText(formatter.format());
+                            formattedCode = formatter.format();
                         }
                     });
+                    editor.setText(formattedCode);
         } else if (id == R.id.settings_menu_button) {
 
             Intent intent = new Intent(MainActivity.this, SettingActivity.class);

e402c016969f0815a24527028e650c4da2375839
Arjuna Kumar Mohanta <101985594+LetsDoNothing@users.noreply.github.com>
2022-05-16 08:28:36 +0530

    Fix

--- app/src/main/java/com/pranav/java/ide/DebugActivity.java
+++ app/src/main/java/com/pranav/java/ide/DebugActivity.java
@@ -4,6 +4,7 @@ import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;

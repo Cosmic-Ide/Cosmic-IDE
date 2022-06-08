1e0f428c06c73dc029c10b07ffc592050746c5b0
PranavPurwar <purwarpranav80@gmail.com>
2022-06-08 23:03:17 +0530

    fix typo

--- app/src/main/java/com/pranav/java/ide/MainActivity.java
+++ app/src/main/java/com/pranav/java/ide/MainActivity.java
@@ -429,7 +429,7 @@ public final class MainActivity extends AppCompatActivity {
                     var edi = new CodeEditor(MainActivity.this);
                     edi.setTypefaceText(Typeface.MONOSPACE);
                     edi.setColorScheme(new SchemeDarcula());
-                    edi.setEditorLanguage(newLanguage());
+                    edi.setEditorLanguage(new JavaLanguage());
                     edi.setTextSize(12);
 
                     try {

02a3795d20188c301645664bf52ed24a75e2394a
Pranav <purwarpranav80@gmail.com>
2022-06-14 20:29:00 +0530

    Use androidx annotations instead of jetbrains one

--- kotlinc/src/main/java/org/jetbrains/kotlin/com/intellij/util/lang/JavaVersion.java
+++ kotlinc/src/main/java/org/jetbrains/kotlin/com/intellij/util/lang/JavaVersion.java
@@ -1,8 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.lang;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import androidx.annotation.NotNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.HashMap;

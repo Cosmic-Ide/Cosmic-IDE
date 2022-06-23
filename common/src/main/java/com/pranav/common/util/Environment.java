a0e0087e50ef64c66b1a9cd513feae708e1ee804
Pranav <purwarpranav80@gmail.com>
2022-06-23 22:15:41 +0530

    Implements javac from androidide

---
+++ common/src/main/java/com/pranav/common/util/Environment.java
@@ -0,0 +1,13 @@
+package com.itsaky.androidide.utils;
+
+import java.io.File;
+/*
+ * Required by nb-javac-android
+ */
+public class Environment {
+    public static File COMPILER_MODULE;
+
+    public static void init(File f) {
+        COMPILER_MODULE = f;
+    }
+}

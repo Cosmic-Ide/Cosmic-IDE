0a764b60ac2b61999932d4211cdc502176be03cb
PranavPurwar <purwarpranav80@gmail.com>
2022-06-29 23:26:34 +0530

    convert TreeJavaFile to kotlin

--- app/src/main/java/com/pranav/java/ide/ui/treeview/helper/TreeCreateNewFileContent.kt
+++ app/src/main/java/com/pranav/java/ide/ui/treeview/helper/TreeCreateNewFileContent.kt
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class $fileName {
     public static void main(String[] args) {
-        System.out.println(\"Hello, World!\");
+        System.out.println("Hello, World!");
     }
 }
         """

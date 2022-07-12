85691f3ca4810d8e3a5fdbe13c5d4aefcda6718a
Pranav <purwarpranav80@gmail.com>
2022-07-12 18:00:48 +0530

    Fix #134 (#136)
    * Update MainActivity.java
    
    * Make use of IllegalStateException instead of a RuntimeException
    
    * Create Environment.kt
    
    * downgrade nb-javac-android
    
    * downgrade nb-javac-android
    
    * downgrade nb-javac android
    
    * fix typo

---
+++ common/src/main/java/com/pranav/common/util/Environment.kt
@@ -0,0 +1,16 @@
+package com.itsaky.androidide.utils
+
+import java.io.File
+
+/*
+ * Required by nb-javac-android
+ */
+ object Environment {
+    @JvmStatic
+    lateinit var COMPILER_MODULE: File
+
+    @JvmStatic
+    fun init(module: File) {
+        COMPILER_MODULE = module
+    }
+}

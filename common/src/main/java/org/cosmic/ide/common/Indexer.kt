97871db9cd7e2e3c21df599453cd79283b5a638e
Pranav <purwarpranav80@gmail.com>
2022-08-21 15:24:59 +0530

    Add 'github' related translations for russian and portugese (brazil)
    translated with duckduckgo

--- common/src/main/java/org/cosmic/ide/common/Indexer.kt
+++ common/src/main/java/org/cosmic/ide/common/Indexer.kt
@@ -70,7 +70,7 @@ class Indexer {
     fun flush() {
         try {
             FileUtil.writeFile(filePath, asString())
-        } catch (e: IOException | JSONException) {
+        } catch (e: Throwable) {
         }
     }
 }

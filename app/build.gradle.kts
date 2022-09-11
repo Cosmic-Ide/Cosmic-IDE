df6d7ddc6d69dbf19a024c60bde0b4fbfaa12bd3
Pranav <purwarpranav80@gmail.com>
2022-09-11 19:58:51 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ android {
                 "plugin.properties",
                 "baksmali.properties",
                 ".api_description",
-                "META-INF/*",
+//                "META-INF/*",
                 "README.md",
                 "SECURITY.md",
                 "about_files/*",

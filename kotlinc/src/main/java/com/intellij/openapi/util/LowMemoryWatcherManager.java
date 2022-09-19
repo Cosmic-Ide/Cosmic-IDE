88a0cd67b9332e652376367d4ee56ecda03e495c
PranavPurwar <purwarpranav80@gmail.com>
2022-09-19 14:38:58 +0530

    Remove duplicate classes

---
+++ kotlinc/src/main/java/com/intellij/openapi/util/LowMemoryWatcherManager.java
@@ -0,0 +1,17 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.util;
+
+import com.intellij.openapi.Disposable;
+import androidx.annotation.NonNull;
+import java.util.concurrent.ExecutorService;
+
+public final class LowMemoryWatcherManager implements Disposable {
+  public LowMemoryWatcherManager(@NotNull ExecutorService backendExecutorService) {
+    
+  }
+
+  @Override
+  public void dispose() {
+    
+  }
+}

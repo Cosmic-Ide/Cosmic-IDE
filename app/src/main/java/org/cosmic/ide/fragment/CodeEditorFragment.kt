969ef371bdb073aa46de97ed00a68ce58e76520c
PranavPurwar <purwarpranav80@gmail.com>
2022-09-21 14:15:43 +0530

    fix constructor paramters

--- app/src/main/java/org/cosmic/ide/fragment/CodeEditorFragment.kt
+++ app/src/main/java/org/cosmic/ide/fragment/CodeEditorFragment.kt
@@ -164,7 +164,7 @@ class CodeEditorFragment : Fragment() {
 
     private fun getKotlinLanguage(): Language {
         try {
-            return KotlinLanguage(binding.editor, (requireActivity() as MainActivity).getProject(), currentFile)
+            return KotlinLanguage(binding.editor, (requireActivity() as MainActivity).getProject(), currentFile, getColorScheme().themeSource)
         } catch (e: IOException) {
             return EmptyLanguage()
         }

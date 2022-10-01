d13658154053278dafd5447c883a3c20aaac6217
PranavPurwar <purwarpranav80@gmail.com>
2022-10-01 23:27:57 +0530

    Migrate to Android Library

--- kotlin-formatter/build.gradle.kts
+++ kotlin-formatter/build.gradle.kts
@@ -1,16 +1,32 @@
 plugins {
-    id(BuildPlugins.JAVA_LIBRARY)
-    id(BuildPlugins.KOTLIN_JAVA)
-    id(BuildPlugins.LINT)
+    id(BuildPlugins.ANDROID_LIBRARY)
+    id(BuildPlugins.KOTLIN_ANDROID)
     id(BuildPlugins.KTLINT)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
+android {
+    namespace = "org.jetbrains.kotlin.idea.formatter"
+    compileSdk = BuildAndroidConfig.COMPILE_SDK_VERSION
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    defaultConfig {
+        minSdk = BuildAndroidConfig.MIN_SDK_VERSION
+        targetSdk = BuildAndroidConfig.TARGET_SDK_VERSION
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }

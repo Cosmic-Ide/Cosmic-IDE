019966a0000c4130093778c09d8f8280d2816b25
PranavPurwar <purwarpranav80@gmail.com>
2022-10-05 10:01:31 +0530

    ...

---
+++ kotlinc/src/main/java/com/intellij/openapi/application/Result.java
@@ -0,0 +1,10 @@
+package com.intellij.openapi.application;
+
+public class Result<T> {
+
+    protected T myResult;
+
+    public final void setResult(T result) {
+        myResult = result;
+    }
+}

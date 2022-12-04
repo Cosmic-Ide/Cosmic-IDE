9588e59954d4d6a7e84cd89bd20723c3b52c1357
TheRemakerMan <farzasz7@gmail.com>
2022-12-04 11:17:01 +0700

    Add summary to some preferences

--- app/src/main/java/org/cosmic/ide/fragment/settings/CompilerSettingsFragment.kt
+++ app/src/main/java/org/cosmic/ide/fragment/settings/CompilerSettingsFragment.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.preference.Preference
+import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
 import org.cosmic.ide.R
 import org.cosmic.ide.ui.preference.showListPreferenceDialog
@@ -14,6 +15,14 @@ class CompilerSettingsFragment :
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.pref_compiler)
+
+        findPreference<EditTextPreference>("program_arguments")?.run {
+            summary = getText()
+            setOnPreferenceChangeListener { preference, newValue ->
+                preference.summary = newValue.toString()
+                true
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

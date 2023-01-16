4e16c27991e7684fc3efbc14bf68abd6a96aaf12
PranavPurwar <purwarpranav80@gmail.com>
2023-01-16 19:07:17 +0530

    Ask user to grant permission manually if ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION cannot be found

--- app/src/main/java/org/cosmic/ide/MainActivity.kt
+++ app/src/main/java/org/cosmic/ide/MainActivity.kt
@@ -3,8 +3,12 @@ package org.cosmic.ide
 import android.Manifest.permission
 import android.content.pm.PackageManager
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
@@ -14,19 +18,23 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import org.cosmic.ide.databinding.ActivityMainBinding
-import org.cosmic.ide.ui.preference.Settings
+import org.cosmic.ide.ui.preference.Settings as AppSettings
 import org.cosmic.ide.util.AndroidUtilities
 import org.cosmic.ide.util.addSystemWindowInsetToPadding
 
 class MainActivity : AppCompatActivity() {
     private val isStoragePermissionsGranted: Boolean
         get() =
-            (ContextCompat.checkSelfPermission(this, permission.READ_EXTERNAL_STORAGE) ==
-                PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, permission.WRITE_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_GRANTED)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                Environment.isExternalStorageManager()
+            } else {
+                (ContextCompat.checkSelfPermission(this, permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, permission.WRITE_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_GRANTED)
+            }
 
-    private val settings = Settings()
+    private val settings = AppSettings()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,11 +63,27 @@ class MainActivity : AppCompatActivity() {
         if (isStoragePermissionsGranted) {
             return
         }
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(permission.WRITE_EXTERNAL_STORAGE, permission.READ_EXTERNAL_STORAGE),
-            1000
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+
+            try {
+                startActivity(
+                    Intent(
+                        Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                        uri
+                    )
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                AndroidUtilities.showToast("Cannot ask for permission. Please grant the permission manually.")
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(permission.WRITE_EXTERNAL_STORAGE, permission.READ_EXTERNAL_STORAGE),
+                1000
+            )
+        }
     }
 
     private fun onStorageDenied() {
@@ -70,11 +94,11 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.grant),
             getString(R.string.dialog_close)
         ) { _, which ->
-            if (which == DialogInterface.BUTTON_POSITIVE) {
-                requestStorage()
-            } else {
+            if (which != DialogInterface.BUTTON_POSITIVE) {
                 finishAffinity()
+                return
             }
+            requestStorage()
         }
     }
 

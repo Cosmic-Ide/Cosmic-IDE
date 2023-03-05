dfb60bdd15385f5b5bd89baa23cddb817b625fa1
PranavPurwar <purwarpranav80@gmail.com>
2023-03-05 16:21:37 +0530

    rm: Remove FCM

--- app/src/main/java/org/cosmic/ide/Analytics.kt
+++ app/src/main/java/org/cosmic/ide/Analytics.kt
@@ -1,17 +1,20 @@
 package org.cosmic.ide
 
 import java.net.URL
+import org.cosmic.ide.activity.BaseActivity
 
-class Analytics {
+object Analytics {
 
     private val PROJECT_ID = "8a8923dd-c722-43b7-b4bf-26483877ea58"
     private val url = "https://app.piratepx.com/ship?"
 
+    @JvmStatic
     fun onAppStarted() {
         URL(url + "p=${ PROJECT_ID }&i=App").openStream()
     }
 
-    fun onActivity(Class activity) {
+    @JvmStatic
+    fun onActivity(activity: BaseActivity) {
         URL(url + "p=${ PROJECT_ID }&i=${ activity.simpleName }").openStream()
     }
 }

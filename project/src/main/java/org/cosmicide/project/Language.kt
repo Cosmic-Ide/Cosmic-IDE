23b9a532991795ee9c246c6579ad6941a7ec610b
TheRemakerMan <farzasz7@gmail.com>
2023-03-10 23:34:21 +0700

    refactor: Improve language class
    Add proper documentation for each function explaining what it does and how it can be used.

--- project/src/main/java/org/cosmicide/project/Language.kt
+++ project/src/main/java/org/cosmicide/project/Language.kt
@@ -1,13 +1,37 @@
 package org.cosmicide.project
 
+/**
+ * A sealed interface representing a programming language.
+ *
+ * @property extension the file extension associated with the language
+ */
 sealed interface Language {
     val extension: String
-    fun getClassFile(name: String, packageName: String): String
+
+    /**
+     * Generates the content of a class file for the language.
+     *
+     * @param name the name of the class
+     * @param packageName the name of the package the class belongs to
+     * @return the generated class file content as a string
+     */
+    fun classFileContent(name: String, packageName: String): String
 }
 
+/**
+ * An object representing the Java programming language.
+ */
 object Java : Language {
     override val extension = "java"
-    override fun getClassFile(name: String, packageName: String): String {
+
+    /**
+     * Generates the content of a Java class file.
+     *
+     * @param name the name of the class
+     * @param packageName the name of the package the class belongs to
+     * @return the generated Java class file content as a string
+     */
+    override fun classFileContent(name: String, packageName: String): String {
         return Templates.javaClass(
             name, packageName, """
             System.out.println("Hello, World!");
@@ -16,9 +40,20 @@ object Java : Language {
     }
 }
 
+/**
+ * An object representing the Kotlin programming language.
+ */
 object Kotlin : Language {
     override val extension = "kt"
-    override fun getClassFile(name: String, packageName: String): String {
+
+    /**
+     * Generates the content of a Kotlin class file.
+     *
+     * @param name the name of the class
+     * @param packageName the name of the package the class belongs to
+     * @return the generated Kotlin class file content as a string
+     */
+    override fun classFileContent(name: String, packageName: String): String {
         return Templates.kotlinClass(
             name, packageName, """
             println("Hello World!")

9f8e666c7b289ad57c9a570eec25719f0a11711b
TheRemakerMan <farzasz7@gmail.com>
2023-03-11 16:34:44 +0700

    feat: move the project loading process to the viewmodel
    Removed the setOnClickListeners(), setUpProjectList(), observeViewModelProjects() and showSnackbarIfNoProjects() methods to reduce the complexity of the onViewCreated() method
    
    The findNavController() request has been moved to a separate navigateToNewProjectFragment() and navigateToEditorFragment() method to reduce repetitive code.
    
    To improve the readability of the code, the if blocks have also been moved to separate methods.

---
+++ app/src/main/java/org/cosmicide/rewrite/model/ProjectViewModel.kt
@@ -0,0 +1,43 @@
+package org.cosmicide.rewrite.model
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.cosmicide.project.Java
+import org.cosmicide.project.Project
+import org.cosmicide.rewrite.util.FileUtil
+import java.io.File
+import java.util.Arrays
+
+class ProjectViewModel : ViewModel() {
+
+    private val _projects = MutableLiveData<List<Project>>()
+    val projects: LiveData<List<Project>> = _projects
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            val projectsList = mutableListOf<Project>()
+            val projectDir = FileUtil.projectDir
+            val directories = projectDir.listFiles { file -> file.isDirectory }
+            if (directories != null) {
+                Arrays.sort(directories, Comparator.comparingLong(File::lastModified).reversed())
+                for (directory in directories) {
+                    projectsList.add(Project(directory, Java))
+                }
+            }
+            withContext(Dispatchers.Main) {
+                _projects.value = projectsList
+                Log.d(TAG, "Projects: $projectsList")
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "ProjectViewModel"
+    }
+}

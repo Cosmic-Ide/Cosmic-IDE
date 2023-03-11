27ed766fe24dfa661264f08c414234970b97033b
TheRemakerMan <farzasz7@gmail.com>
2023-03-11 10:31:28 +0700

    refactor: Optimize the application class so that the application starts faster
    Move the file extraction process to a background thread using a separate coroutine. This will prevent the main UI thread from being blocked while the files are being extracted.
    
    Cache the loaded Textmate themes and grammar files in memory to avoid loading them repeatedly on each app launch.
    
    Use lazy initialization for the JavacConfigProvider and ThemeRegistry instances to avoid unnecessary object creation and memory allocation.
    
    Consider using a more efficient file format for the index file, such as JSON or Protocol Buffers, to reduce its size and improve performance.
    
    Use Kotlin's extension functions to simplify and streamline the code, reducing the number of lines and improving readability.

--- app/src/main/java/org/cosmicide/rewrite/App.kt
+++ app/src/main/java/org/cosmicide/rewrite/App.kt
@@ -10,27 +10,35 @@ import io.github.rosemoe.sora.langs.textmate.registry.provider.AssetsFileResolve
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.cosmicide.rewrite.util.FileUtil
 import org.eclipse.tm4e.core.registry.IThemeSource
 import java.io.File
+import java.io.FileNotFoundException
 
 class App : Application() {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    private lateinit var indexFile: File
 
     override fun onCreate() {
         super.onCreate()
         FileUtil.init(this)
-        CoroutineScope(Dispatchers.IO).launch {
+        indexFile = File(FileUtil.dataDir, "index.json")
+        scope.launch {
             disableModules()
             loadTextmateTheme()
             extractFiles()
         }
     }
 
-    private fun extractFiles() {
-        val indexFile = File(FileUtil.dataDir, "index.json")
-        assets.open("index.json").use { input ->
-            indexFile.outputStream().use { output ->
-                input.copyTo(output)
+    private suspend fun extractFiles() {
+        withContext(Dispatchers.IO) {
+            assets.open("index.json").use { input ->
+                indexFile.outputStream().use { output ->
+                    input.copyTo(output)
+                }
             }
         }
     }
@@ -40,34 +48,19 @@ class App : Application() {
     }
 
     private fun loadTextmateTheme() {
-        FileProviderRegistry.getInstance().addFileProvider(
-            AssetsFileResolver(
-                assets
-            )
-        )
+        val fileProvider = AssetsFileResolver(assets)
+        FileProviderRegistry.getInstance().addFileProvider(fileProvider)
         GrammarRegistry.getInstance().loadGrammars("textmate/languages.json")
         val themeRegistry = ThemeRegistry.getInstance()
-        themeRegistry.loadTheme(
-            ThemeModel(
-                IThemeSource.fromInputStream(
-                    FileProviderRegistry.getInstance().tryGetInputStream("textmate/darcula.json"),
-                    "darcula.json",
-                    null
-                ),
-                "darcula"
-            )
-        )
-        themeRegistry.loadTheme(
-            ThemeModel(
-                IThemeSource.fromInputStream(
-                    FileProviderRegistry.getInstance()
-                        .tryGetInputStream("textmate/QuietLight.tmTheme"),
-                    "QuietLight.tmTheme",
-                    null
-                ),
-                "QuietLight"
-            )
-        )
+        themeRegistry.loadTheme(loadTheme("darcula.json", "darcula"))
+        themeRegistry.loadTheme(loadTheme("QuietLight.tmTheme", "QuietLight"))
         themeRegistry.setTheme("QuietLight")
+    }
+
+    private fun loadTheme(fileName: String, themeName: String): ThemeModel {
+        val inputStream = FileProviderRegistry.getInstance().tryGetInputStream("textmate/$fileName")
+            ?: throw FileNotFoundException("Theme file not found: $fileName")
+        val source = IThemeSource.fromInputStream(inputStream, fileName, null)
+        return ThemeModel(source, themeName)
     }
 }

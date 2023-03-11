3b53a495fe70dc4e6c788567cb4eb9bffbde30b6
PranavPurwar <purwarpranav80@gmail.com>
2023-03-11 15:10:26 +0530

    feat: generate source files with javapoet and kotlinpoet
    Signed-off-by: PranavPurwar <purwarpranav80@gmail.com>

--- project/build.gradle.kts
+++ project/build.gradle.kts
@@ -7,3 +7,8 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation("com.squareup:javapoet:1.13.0")
+    implementation("com.squareup:kotlinpoet:1.12.0")
+}

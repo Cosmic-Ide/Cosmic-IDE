9aa605195b1721065b6a6e11893250753f84ec6c
PranavPurwar <purwarpranav80@gmail.com>
2023-03-29 17:00:31 +0530

    fix: check for internet connection

--- app/src/main/java/org/cosmic/ide/Analytics.kt
+++ app/src/main/java/org/cosmic/ide/Analytics.kt
@@ -1,5 +1,9 @@
 package org.cosmic.ide
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import java.net.URL
 import org.cosmic.ide.activity.BaseActivity
 
@@ -10,11 +14,41 @@ object Analytics {
 
     @JvmStatic
     fun onAppStarted() {
+        if (!isNetworkAvailable(App.context)) return
         URL(url + "p=${ PROJECT_ID }&i=App").openStream()
     }
 
     @JvmStatic
     fun onActivity(activity: BaseActivity) {
+        if (!isNetworkAvailable(activity)) return
         URL(url + "p=${ PROJECT_ID }&i=${ activity::class.simpleName }").openStream()
+    }
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
     }
 }

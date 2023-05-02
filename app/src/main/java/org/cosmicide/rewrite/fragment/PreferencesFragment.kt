c413f2f1d31a14b97727e5c476bb036ea446aa76
TheRemakerMan <farzasz7@gmail.com>
2023-05-02 19:59:50 +0700

    style(preferences): remove unnecessary layout animation
    Signed-off-by: TheRemakerMan <farzasz7@gmail.com>

--- app/src/main/java/org/cosmicide/rewrite/fragment/PreferencesFragment.kt
+++ app/src/main/java/org/cosmicide/rewrite/fragment/PreferencesFragment.kt
@@ -1,13 +1,9 @@
 package org.cosmicide.rewrite.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import androidx.fragment.app.FragmentTransaction
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.recyclerview.widget.RecyclerView
 import org.cosmicide.rewrite.BuildConfig
 import org.cosmicide.rewrite.R
 
@@ -39,11 +35,5 @@ class PreferencesFragment : PreferenceFragmentCompat() {
                 true
             }
         }
-    }
-
-    override fun onCreateRecyclerView(inflater: LayoutInflater, parent: ViewGroup, savedInstanceState: Bundle?): RecyclerView {
-        val recyclerView = super.onCreateRecyclerView(inflater, parent, savedInstanceState)
-        recyclerView.layoutAnimation = AnimationUtils.loadLayoutAnimation(requireContext(), R.anim.preference_layout_fall_down)
-        return recyclerView
     }
 }

9f418312f325109925c562d413fc02103e795e8c
Pranav <purwarpranav80@gmail.com>
2023-08-14 06:38:16 +0530

    Update MainActivity.kt
    java caused this lol

--- app/src/main/kotlin/org/cosmicide/rewrite/MainActivity.kt
+++ app/src/main/kotlin/org/cosmicide/rewrite/MainActivity.kt
@@ -89,7 +89,7 @@ class MainActivity : AppCompatActivity() {
             if (Shizuku.shouldShowRequestPermissionRationale()) {
                 requestPermission()
             }
-        } catch (Throwable e) {
+        } catch (e: Throwable) {
             e.printStackTrace()
         }
     }

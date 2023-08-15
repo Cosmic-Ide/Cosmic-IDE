c96ec3416fa48bf0af57271f2a134df28855628d
PranavPurwar <purwarpranav80@gmail.com>
2023-08-15 15:38:14 +0530

    Fix crash on clicking directory multiple times (fixes #93)
    Signed-off-by: PranavPurwar <purwarpranav80@gmail.com>

--- feature/TreeView/src/main/kotlin/com/widget/treeview/TreeView.kt
+++ feature/TreeView/src/main/kotlin/com/widget/treeview/TreeView.kt
@@ -22,7 +22,21 @@ data class Node<T>(
     var child: List<Node<T>>? = null,
     var isExpand: Boolean = false,
     var level: Int = 0
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Node<*>
+
+        return value == other.value && parent == other.parent && child == other.child && isExpand == other.isExpand && level == other.level
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+}
 
 object TreeView {
 
